853d4fc14e10106e12b7978e318ae2e43c9bb80b
makspaderin <mvpaderin@gmail.com>
2017-04-13 10:39:06 +0300

    Attempt with time 2

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.time_zone = 'Eastern European Time'
+config.time_zone = 'Eastern Time (US & Canada)'
 config.active_record.default_timezone = :local
 Paperclip.options[:command_path] = "/usr/local/bin/"
 
