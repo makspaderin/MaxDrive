76a1090b065edd67adcc4eccf5ca4b94030de08b
makspaderin <mvpaderin@gmail.com>
2017-04-13 09:34:43 +0300

    Fix #3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,12 @@ class UsersController < ApplicationController
 	def new
 		@user = User.new
 	end
+
 	def create
 		@user = User.new(user_params)
 		if @user.save
 			session[:user_id] = @user.id
 			redirect_to '/'
-			end
 		else
 			redirect_to '/signup'
 		end
@@ -31,3 +31,4 @@ class UsersController < ApplicationController
 	def user_params
 		params.require(:user).permit(:first_name, :last_name, :username, :password)
 	end
+end
