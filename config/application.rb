a85cc042d2504884766864cd9446685e00f00cfe
makspaderin <mvpaderin@gmail.com>
2017-04-13 09:38:17 +0300

    Added time

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,8 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+config.time_zone = 'Eastern European Time'
+config.active_record.default_timezone = :local
 
 module Maxdrive
   class Application < Rails::Application
